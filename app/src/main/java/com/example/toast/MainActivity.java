cc0c24aa9c7da3b49e4556108bea3294bd3b1b9f
Yehor Savchuk <95549799+YehorSk@users.noreply.github.com>
2023-08-02 15:53:12 +0200

    Seconds commit

--- app/src/main/java/com/example/toast/MainActivity.java
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button showBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUI();
+        showBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CustomToast();
+            }
+        });
+
+    }
+
+    private void CustomToast(){
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.toast_layout,this.findViewById(R.id.customToast));
+
+        Toast toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(view);
+        toast.show();
+
+    }
+
+    private void setUI(){
+        showBtn = findViewById(R.id.button);
     }
 }
